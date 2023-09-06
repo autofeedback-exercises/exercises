4c227828eb70fac1c37e148955f5dcad5c03b671
Andrew Brown <andrew.brown@qub.ac.uk>
2023-09-06 14:51:27 +0100

    fixed queuing-tool-2 tests

--- SOR3012/Queuing-tool-exercises/queuing-tool-2/.lesson/test_main.py
+++ SOR3012/Queuing-tool-exercises/queuing-tool-2/.lesson/test_main.py
@@ -8,8 +8,22 @@ except:
     import AutoFeedback.varchecks as vc
 
 import unittest
-from main import *
+#from main import *
 
 class UnitTests( unittest.TestCase ): 
-   def test_nservers(self):
-      assert( vc.check_vars("qn.edge2queue[0].num_servers",3) )
+    def test_nservers(self):
+        try:
+            from main import qn
+            edge2queue = qn.edge2queue
+        except ImportError:
+            assert vc.check_vars("qn", 0)
+        except AttributeError:
+            assert vc.check_vars("qn.edge2queue", 0)
+        try:
+            num_servers = edge2queue[0].num_servers
+        except AttributeError:
+            assert vc.check_vars("qn.edge2queue[0].num_servers", 0)
+
+        assert vc.check_vars(num_servers, 3,
+                             printname="qn.edge2queue[0].num_servers")
+
