56d09c9e1b2c9bdf30efd1265547ddc4337d22aa
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro-4.local>
2025-08-28 17:22:30 +0100

    First attempt at Lattice Gas exercises

---
+++ New-MTH4332/LatticeGas/main.py
@@ -0,0 +1,295 @@
+# Exercise 1
+nspins = 10 
+allup = np.ones(nspins)
+alldown = -1*np.ones(nspins)
+alternating = np.ones(nspins)
+for i in range(nspins) :
+    if i%2==0 : alternating[i]=-1
+
+# Exercise 2
+def rising_states( nspins, nstates ) :
+    spins = np.zeros( nspins )
+    for i in range(nspins) :
+        spins[i] = i%nstates
+    return spins
+def lowering_states( nspins, nstates ) :
+    spins = np.zeros( nspins )
+    for i in range(nspins) :
+        spins[i] = nstates - 1 - i%nstates
+    return spins
+def updown_states( nspins, nstates ) :
+    spins = np.zeros( nspins )
+    for i in range(nspins) :
+        base = np.floor( i / nstates )
+        if base%2==0 : spins[i] = i%nstates
+        else : spins[i] = nstates - 1 - i%nstates
+    return spins
+
+# Exercise 3
+def hamiltonian( spins, H ) :
+  energy = 0
+  # Your code goes here
+  return -H*sum(spins)
+allup, alldown = np.ones(10), -1*np.ones(10)
+print( "ENERGY FOR ALL SPIN UP", hamiltonian( allup, 1 ) )
+print( "ENERGY FOR ALL SPIN DOWN", hamiltonian( alldown, 1 ) )
+
+# Exercise 4
+def hamiltonian( coords ) :
+  energy = 0
+  # Your code goes here
+  for c in coords :
+      if c==1 : energy = energy + 2
+      elif c==2 : energy = energy + 3
+  return energy
+allzero, allone, alltwo = np.zeros(10), np.ones(10), 2*np.ones(10)
+print( "ENERGY FOR ALL ZERO", hamiltonian( allzero ) )
+print( "ENERGY FOR ALL ONE", hamiltonian( allone ) )
+print( "ENERGY FOR ALL TWO", hamiltonian( alltwo ) )
+
+# Exercise 5
+def hamiltonian( coords ) :
+  energy = 0
+  # Your code goes here
+  for c in coords :
+      if c==1 or c==2 : energy = energy + 1
+      elif c==3 : energy = energy + 2
+  return energy
+allzero, allone, alltwo, allthree = np.zeros(10), np.ones(10), 2*np.ones(10), 3*np.ones(10)
+print( "ENERGY FOR ALL ZERO", hamiltonian( allzero ) )
+print( "ENERGY FOR ALL ONE", hamiltonian( allone ) )
+print( "ENERGY FOR ALL TWO", hamiltonian( alltwo ) )
+print( "ENERGY FOR ALL THREE", hamiltonian( allthree ) )
+
+# Exercise 6
+def getBinary( N ) : 
+  # Your code goes here
+  bina = np.zeros(6)
+  for i in range(6) :
+     pp = 2**(5-i)
+     bina[i] = np.floor( N / pp )
+     N = N - bina[i]*pp
+  return bina
+for i in range(16) :
+  print("The binary representation of", i, "is", getBinary(i) )
+
+# Exercise 7
+def convertToBase( N, base ) :
+  # Your code goes here
+  state, v = np.zeros(7), N
+  for i in range(7) :
+      vv = base**(6-i)
+      state[i] = np.floor( v / vv )
+      v = v - state[i]*vv
+  return state
+# This will print some output that will allow you to test your function
+for b in range(2,10) :
+  for n in range(10) :
+    print("The number", n, "in base", b, "is", convertToBase(n,b) )
+
+# Exercise 8
+def microstate_energies( N ) : 
+   # Create an array to hold the energies of all the microstates
+   energies = np.zeros( 2**N )
+   for i in range(2**N) :
+       # Generate coordinates of particles for ith microstate
+       val, coords = i, np.zeros(N)
+       for j in range(N) :
+           ppp = 2**(N-1-j)
+           coords[j] = np.floor( val / ppp )
+           val = val - ppp*coords[j]
+           if coords[j]==0 : coords[j] = -1
+       # Evaluate the energy of the microstate and store it in the array that we will return
+       energies[i] = sum(coords)
+   # Return the array that holds the energies of the microstates
+   return energies
+
+# Exercise 9
+def hamiltonian( spins, H ) : 
+  energy = -H*sum(spins)
+  return energy
+def partitionfunction( N, H, T ) :
+  Z = 0
+  # Your code to calculate the partition function goes here
+  for k in range(2**N) :
+      val, spins = k, np.zeros(N)
+      for i in range(N) :
+          ppp = 2**(N-1-i)
+          spins[i] = np.floor( val / ppp )
+          val = val - spins[i]*ppp
+          if spins[i]==0 : spins[i] = -1
+      Z = Z + np.exp( -hamiltonian( spins, H ) / T )
+  return Z
+# Calculate the partition function for a system of 5 spins 
+# with no external field at a temperature of 0.1
+print( partitionfunction(5,0,0.1) )
+# Calculate the paritition function for a system of 6 spins 
+# with a magnetic field strength of 1 at a temperature of 0.5
+print( partitionfunction(6,1,0.5) )
+
+# Exercise 10
+def hamiltonian( spins, H ) :
+  eng = -H*sum(spins)
+  return eng
+# Generate an index for each microstate
+indices = np.zeros(2**8)
+for i in range(2**8) : indices[i] = i
+energies = np.zeros(2**8)
+for index in indices :
+  spins, ind = np.zeros(8), index
+  # Your code to convert the integer index to the corresponding spin coordinates goes here
+  for j in range(8) :
+      ppp = 2**(7-j)
+      spins[j] = np.floor( ind / ppp )
+      ind = ind - spins[j]*ppp
+      if spins[j]==0 : spins[j] = -1
+  energies[int(index)] = hamiltonian( spins, 1 )
+# This will plot the energies of the configurations against their numerical indexes. 
+plt.plot( indices, energies, 'ko')
+plt.xlabel("numerical index")
+plt.ylabel("energy")
+plt.show()
+# This code is required for the Automated feedback, don't delete it!
+fighand = plt.gca()
+
+# Exercise 11
+def hamiltonian( spins, H ) :
+  eng = -H*sum(spins)
+  return eng
+# Create a list of the posssible values the energy can take
+energies = np.zeros(9)
+for i in range(9) : energies[i] = -8+i*2
+# Create a list that will hold the number of microstates with each energy 
+number_of_microstates = np.zeros(9)
+# Your code to do the loop over all the microstates and to count how many times each 
+# of the energy values appear goes here
+for i in range(2**8) :
+    vv, spins = i, np.zeros(8)
+    for j in range(8) :
+        ppp = 2**(7-j)
+        spins[j] = np.floor( vv / ppp )
+        vv = vv - spins[j]*ppp
+        if spins[j]==0 : spins[j] = -1
+    eng = hamiltonian( spins, 1 )
+    number_of_microstates[ int((eng + 8)/2) ] = number_of_microstates[ int((eng + 8)/2) ] + 1
+
+# This will plot the possible values for the energy against the number of microstates with 
+# that particular energy.
+plt.bar( energies, number_of_microstates, width=0.1 )
+plt.xlabel("energy")
+plt.ylabel("Number of microstates")
+plt.show()
+# This code is required for the Automated feedback, don't delete it!
+fighand = plt.gca()
+
+# Exercise 12
+def hamiltonian( spins, H ) :
+  eng = -H*sum(spins)
+  return eng
+# Create a list of the posssible values the energy can take
+energies = np.zeros(9)
+for i in range(9) : energies[i] = -8+i*2
+# Create a list that will hold the number of microstates with each energy 
+number_of_microstates = np.zeros(9)
+# Your code to do the loop over all the microstates and to count how many times each 
+# of the energy values appear goes here
+for i in range(2**8) :
+    vv, spins = i, np.zeros(8)
+    for j in range(8) :
+        ppp = 2**(7-j)
+        spins[j] = np.floor( vv / ppp )
+        vv = vv - spins[j]*ppp
+        if spins[j]==0 : spins[j] = -1
+    eng = hamiltonian( spins, 1 )
+    number_of_microstates[ int((eng + 8)/2) ] = number_of_microstates[ int((eng + 8)/2) ] + 1
+
+entropy = np.log( number_of_microstates )
+# This will plot the possible values for the energy against the number of microstates with 
+# that particular energy.
+plt.bar( energies, entropy, width=0.1 )
+plt.xlabel("energy")
+plt.ylabel("Entropy")
+plt.show()
+# This code is required for the Automated feedback, don't delete it!
+fighand = plt.gca()
+
+# Exercise 13
+def hamiltonian( spins, H ) : 
+  energy = -H*sum(spins)
+  return energy
+def ensemble_average( N, H, T ) :
+  numerator, Z = 0, 0
+  # Your code to calculate the ensemble average goes here
+  for i in range(2**N) :
+      v, spins = i, np.zeros(N)
+      for j in range(N) :
+          ppp = 2**(N-1-j)
+          spins[j] = np.floor( v / ppp )
+          v = v - ppp*spins[j]
+          if spins[j]==0 : spins[j] = -1
+      e = hamiltonian( spins, H )
+      bwe = np.exp( -e/T )
+      numerator = numerator + e*bwe
+      Z = Z + bwe
+  return numerator / Z
+# Calculate the ensemble average of the energy for a system of 5 spins 
+# with an external field of 1 at a temperature of 0.1
+print( ensemble_average(5,0,1.1) )
+# Calculate the ensemble average for a system of 6 spins 
+# with a magnetic field strength of 1 at a temperature of 0.5
+print( ensemble_average(6,1,0.5) )
+
+# Exercise 14
+def hamiltonian( spins, H ) :
+  energy = -H*sum(spins)
+  return energy
+def ensemble_average( N, H, T ) :
+  numerator, Z = 0, 0
+  # Your code to calculate the partition function goes here
+  for i in range(2**N) :
+      v, spins = i, np.zeros(N)
+      for j in range(N) :
+          ppp = 2**(N-1-j)
+          spins[j] = np.floor( v / ppp )
+          v = v - ppp*spins[j]
+          if spins[j]==0 : spins[j] = -1
+      e = hamiltonian( spins, H )
+      bwe = np.exp( -e/T )
+      numerator = numerator + e*bwe
+      Z = Z + bwe
+  return numerator / Z
+energies, k = np.zeros(15), 0
+temperatures = np.linspace(0.1,3.1,15)
+for temp in temperatures :
+    energies[k] = ensemble_average(8, 1., temp)
+    k = k + 1
+plt.plot( temperatures, energies, 'k-' )
+plt.xlabel("temperature / arbitrary units")
+plt.ylabel("average energy / arbitrary units")
+plt.show()
+# This code is required for the Automated feedback, don't delete it!
+fighand = plt.gca()
+
+# Exercise 15
+# Define symbols that we will use to represent:
+# N = number of particles
+# B = 1/T where T is temperature
+N, B = sy.symbols("N B")
+# One particle partition function
+Z1 = 1 + sy.exp(-B) + sy.exp(-3*B )
+# N particle partition function
+ZN = Z1**N
+# Now ensemble average of the energy 
+# sy.diff calculates the derivative with respect to B
+E = -sy.diff( sy.log(ZN), B )
+print("The ensemble average for the energy can be calculated using", E )
+# Lets now draw a graph of this result using matplotlib and NumPy
+# We are setting H=1 and N=8 here
+temperatures = np.linspace(0.1,3,100)
+energies = -8*( -np.exp(-1/temperatures ) - 3*np.exp(-3/temperatures) )/ (1  + np.exp(-1/temperatures) + np.exp(-3/temperatures) )
+plt.plot( temperatures, energies, 'k-' )
+plt.xlabel('temperature')
+plt.ylabel('average energy')
+plt.show()
+# This code is required for the Automated feedback, don't delete it!
+fighand = plt.gca()
---
+++ New-MTH4332/LatticeGas/testsrc/test_main.py
@@ -0,0 +1,366 @@
+try:
+    import AutoFeedback.varchecks as vc
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    import AutoFeedback.varchecks as vc
+
+from AutoFeedback.funcchecks import check_func
+from AutoFeedback.plotclass import line
+from AutoFeedback.plotchecks import check_plot
+from AutoFeedback.utils import get_internal
+import unittest
+
+class UnitTests(unittest.TestCase) :
+    def test_alternating(self) :
+        alt = np.ones(nspins)
+        for i in range(nspins) :
+            if i%2==0 : alt[i] = -1
+        assert( vc.check_vars("alternating", alt ) ) 
+            
+    def test_spinDown(self) :
+        assert( vc.check_vars("alldown", -1*np.ones(nspins) ) )
+        
+    def test_spinUp(self) :
+        assert( vc.check_vars("allup", np.ones(nspins) ) )
+
+    def test_rising(self) :
+        inputs, variables = [], []
+        for i in range(6,21) :
+            for j in range(3,5) :
+                inputs.append((i,j))
+                spins = np.zeros(i)
+                for k in range(i) : spins[k] = k%j 
+                variables.append( spins )
+        assert( check_func('rising_states',inputs, variables ) )         
+   
+    def test_lowering(self) :
+        inputs, variables = [], []
+        for i in range(6,21) :
+            for j in range(3,5) :
+                inputs.append((i,j))
+                spins = np.zeros(i)
+                for k in range(i) : spins[k] = j - 1 - k%j 
+                variables.append( spins )
+        assert( check_func('lowering_states',inputs, variables ) )
+        
+    def test_updown(self) :
+        inputs, variables = [], []
+        for i in range(6,21) :
+            for j in range(3,5) :
+                inputs.append((i,j))
+                spins = np.zeros(i)
+                for k in range(i) :
+                    kb = np.floor( k / j )
+                    if kb%2==0 : spins[k] = k%j
+                    else : spins[k] = j - 1 - k%j 
+                variables.append( spins )
+        assert( check_func('updown_states',inputs, variables ) )
+
+    def test_eng1(self) :
+        inputs, outputs = [], [] 
+        for n in range(3,6) : 
+            for i in range(2**n) :
+                num, spins = i, np.zeros(n)
+                for j in range(n) :
+                    spins[j] = np.floor( num / 2**(n-1-j) )
+                    num = num - spins[j]*2**(n-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                for f in range(1,4) : 
+                    inputs.append((spins,f))
+                    outputs.append( -f*sum(spins) )
+        assert( check_func('hamiltonian', inputs, outputs ) )   
+
+    def test_eng2(self) :
+        inputs, outputs = [], [] 
+        for n in range(3,6) : 
+            for i in range(3**n) :
+                num, spins, eng = i, np.zeros(n), 0
+                for j in range(n) :
+                    spins[j] = np.floor( num / 3**(n-1-j) )
+                    num = num - spins[j]*3**(n-1-j)
+                    if spins[j]==1 : eng = eng+2 
+                    elif spins[j]==2 : eng = eng+3
+                inputs.append((spins,))
+                outputs.append( eng )
+        assert( check_func('hamiltonian', inputs, outputs ) )   
+
+    def test_eng3(self) :
+        inputs, outputs = [], [] 
+        for n in range(3,6) : 
+            for i in range(4**n) :
+                num, spins, eng = i, np.zeros(n), 0
+                for j in range(n) :
+                    spins[j] = np.floor( num / 4**(n-1-j) )
+                    num = num - spins[j]*4**(n-1-j)
+                    if spins[j]==1 or spins[j]==2 : eng = eng+1 
+                    elif spins[j]==3 : eng = eng+2
+                inputs.append((spins,))
+                outputs.append( eng )
+        assert( check_func('hamiltonian', inputs, outputs ) )   
+
+    def test_function1(self) :
+        inputs, outputs = [], []
+        for i in range(20) :
+            val = np.random.randint(0,63)
+            inputs.append((val,))
+            oval = np.zeros(6)
+            for j in range(6) : 
+                ppp = 2**(5-j)
+                oval[j] = int(np.floor( val / ppp ) )
+                val = val - oval[j]*ppp
+            outputs.append( oval )
+        assert( fc.check_func('getBinary', inputs, outputs ) ) 
+
+    def test_function2(self) :
+        inputs, outputs = [], []  
+        for b in range(2,11) :
+            for i in range(20) :
+                val = np.random.randint(0,127)
+                inputs.append((val,b,))
+                outval = np.zeros(7)
+                for j in range(7) : 
+                    ppp = b**(6-j)
+                    outval[j] = int(np.floor( val / ppp ) )
+                    val = val - outval[j]*ppp
+                outputs.append( outval )
+        assert( fc.check_func('convertToBase', inputs, outputs ) )
+
+    def test_function3(self) :
+        inputs, outputs = [], []  
+        for n in range(4,9) :
+            energies = np.zeros(2**n)
+            for k in range(2**n) : 
+                val = k
+                for j in range(n) :
+                    ppp = 2**(n-1-j)
+                    coord = np.floor( val / ppp ) 
+                    val = val - coord*ppp
+                    if( coord==0 ) : energies[k] = energies[k] - 1
+                    else :  energies[k] = energies[k] + 1
+            inputs.append((n,))
+            outputs.append( energies )
+        assert( fc.check_func('microstate_energies', inputs, outputs ) )
+
+    def test_partition_function(self) : 
+        inputs, outputs = [], []  
+        for k in range(5,8) :
+            pfunc1, pfunc2, pfunc3 = 0, 0, 0
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                pfunc1 = pfunc1 + np.exp( -hamiltonian( spins, 1 ) / 0.5 )
+                pfunc2 = pfunc2 + np.exp( -hamiltonian( spins, 1 ) / 0.1 )
+                pfunc3 = pfunc3 + np.exp( -hamiltonian( spins, 2 ) / 0.8 )
+            inputs.append((k,1,0.5,))
+            outputs.append( pfunc1 )
+            inputs.append((k,-1,0.1,))
+            outputs.append( pfunc2 )
+            inputs.append((k,2,0.8,))
+            outputs.append( pfunc3 )
+        assert( check_func('partitionfunction', inputs, outputs ) )
+            
+    def test_hamiltonian( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :  
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) : 
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )
+
+    def test_hamiltonian1( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) :
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )
+        
+    def test_graph2(self) :
+        xvals = np.linspace(0,2**8-1,2**8)
+        yvals = np.zeros( 2**8 )
+        for i in range(2**8) :
+            val = i
+            for j in range(8) :
+                ppp = 2**(7-j)
+                spin = np.floor( val / ppp )
+                val = val - spin*ppp
+                if spin==0 : yvals[i] = yvals[i] + 1
+                else : yvals[i] = yvals[i] - 1
+        
+        line1 = line(xvals, yvals)
+        
+        axislabels=["numerical index","energy"] 
+        assert( check_plot( [line1], explabels=axislabels, explegend=False,output=True) )
+   
+    def test_hamiltonian2( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) :
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )
+        
+    def test_graph4(self) :
+        xvals = np.linspace( -8, 8, 9 )
+        yvals = np.zeros(9)
+        for i in range(2**8) :
+            num, spins = i, np.zeros(8)
+            for j in range(8) :
+                spins[j] = np.floor( num / 2**(7-j) )
+                num = num - spins[j]*2**(7-j)
+                if spins[j]==0 : spins[j] = -1
+            binn = int( ( hamiltonian(spins, 1)  + 8 ) / 2 )
+            yvals[binn] = yvals[binn] + 1
+        
+        line1 = line( xvals, yvals )
+        axislabels = [ "energy", "Number of microstates" ]
+        assert( check_plot([],exppatch=line1,explabels=axislabels,explegend=False,output=True) ) 
+
+    def test_hamiltonian5( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) :
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )
+        
+    def test_graph3(self) :
+        xvals = np.linspace( -8, 8, 9 )
+        yvals = np.zeros(9)
+        for i in range(2**8) :
+            num, spins = i, np.zeros(8)
+            for j in range(8) :
+                spins[j] = np.floor( num / 2**(7-j) )
+                num = num - spins[j]*2**(7-j)
+                if spins[j]==0 : spins[j] = -1
+            binn = int( ( hamiltonian(spins, 1)  + 8 ) / 2 )
+            yvals[binn] = yvals[binn] + 1
+        
+        line1 = line( xvals, np.log(yvals) )
+        axislabels = [ "energy", "Entropy" ]
+        assert( check_plot([],exppatch=line1,explabels=axislabels,explegend=False,output=True) ) 
+
+    def test_ensemble_average(self) :
+        inputs, outputs = [], [] 
+        for k in range(5,8) :
+            Z, numer = np.zeros(9), np.zeros(9)
+            for i in range(2**k) :
+                num, spins = i, k*[0]
+                for j in range(k) :
+                   spins[j] = np.floor( num / 2**(k-1-j) )
+                   num = num - spins[j]*2**(k-1-j)
+                   if spins[j]==0 : spins[j] = -1
+                j = 0 
+                for h in range(-1,2) : 
+                    eee = hamiltonian( spins, h )
+                    for t in [0.5,1.0,2.0] : 
+                        bweight = np.exp( -eee / t )
+                        numer[j] = numer[j] + eee*bweight
+                        Z[j] = Z[j] + bweight
+                        j = j + 1 
+            j=0
+            for h in range(-1,2) : 
+                for t in [0.5,1.0,2.0] :
+                    inputs.append((k,h,t,)) 
+                    outputs.append(numer[j]/Z[j])
+                    j = j + 1
+        assert( check_func('ensemble_average', inputs, outputs ) )
+        
+    def test_hamiltonian3( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) :
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )    
+
+    def test_hamiltonian4( self ) :
+        inputs, outputs = [], []
+        for k in range(5,8) :
+            for i in range(2**k) :
+                num, spins = i, np.zeros(k)
+                for j in range(k) :
+                    spins[j] = np.floor( num / 2**(k-1-j) )
+                    num = num - spins[j]*2**(k-1-j)
+                    if spins[j]==0 : spins[j] = -1
+                sumspins = sum( spins )
+                for j in range(-3,4) :
+                    if j==0 : continue
+                    inputs.append((spins,j,))
+                    outputs.append( -j*sumspins )
+        assert( check_func('hamiltonian', inputs, outputs ) )
+            
+    def test_graph5(self) :
+        xvals = get_internal('temperatures')
+        yvals, k = np.zeros(len(xvals)), 0
+        for t in xvals :
+            n, z = 0, 0
+            for i in range(2**8) :
+                val, eng = i, 0
+                for j in range(8) :
+                    ppp = 2**(7-j)
+                    spin = np.floor( val / ppp )
+                    val = val - spin*ppp
+                    if spin==0 : eng = eng + 1
+                    else : eng = eng - 1
+                bw = np.exp(-eng/t)
+                n, z = n + eng*bw, z + bw
+            yvals[k] = n / z
+            k = k + 1
+        
+        line1 = line(xvals, yvals)
+        axislabels=["temperature / arbitrary units","average energy / arbitrary units"]
+        assert( check_plot( [line1], explabels=axislabels, explegend=False,output=True) )
+
+    def test_graph1(self) :
+        xvals = get_internal('temperatures')
+        yvals = -8*( -np.exp(-1/xvals) - 3*np.exp(-3/xvals) )/ (1  + np.exp(-1/xvals) + np.exp(-3/xvals) )
+        line1 = line(xvals, yvals)
+        axislabels=["temperature","average energy"]
+        assert( check_plot( [line1], explabels=axislabels, explegend=False,output=True) )
