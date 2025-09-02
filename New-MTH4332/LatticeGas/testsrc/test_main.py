e4e95329da1f57fe42aee1de8a755b33fd437aee
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro-4.local>
2025-09-02 11:13:29 +0100

    Fixed error in self marking system

--- New-MTH4332/LatticeGas/testsrc/test_main.py
+++ New-MTH4332/LatticeGas/testsrc/test_main.py
@@ -18,15 +18,18 @@ import unittest
 
 class UnitTests(unittest.TestCase) :
     def test_alternating(self) :
+        nspins = get_internal("nspins")
         alt = np.ones(nspins)
         for i in range(nspins) :
             if i%2==0 : alt[i] = -1
         assert( vc.check_vars("alternating", alt ) ) 
             
     def test_spinDown(self) :
+        nspins = get_internal("nspins")
         assert( vc.check_vars("alldown", -1*np.ones(nspins) ) )
         
     def test_spinUp(self) :
+        nspins = get_internal("nspins")
         assert( vc.check_vars("allup", np.ones(nspins) ) )
 
     def test_rising(self) :
