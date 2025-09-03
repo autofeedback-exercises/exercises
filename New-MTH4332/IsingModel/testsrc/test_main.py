8afd315284cb5818a41510367955d2429eefdbca
Gareth Aneurin Tribello <garethtribello@gareths-mbp-4.mp.qub.ac.uk>
2025-09-03 12:18:48 +0100

    Fixing Ising marking

--- New-MTH4332/IsingModel/testsrc/test_main.py
+++ New-MTH4332/IsingModel/testsrc/test_main.py
@@ -73,7 +73,7 @@ class UnitTests(unittest.TestCase) :
             spins = np.ones([N,N])
             inputs.append((spins,))
             myvar = randomvar( (N-1)/2, variance=(N*N-1)/12, vmin=0, vmax=N-1, isinteger=True, nsamples=100 )
-            outputs.append((myvar,))
+            outputs.append( myvar )
         assert( check_func("chooseSpin", inputs, outputs ) )
 
     def test_mag(self) : 
