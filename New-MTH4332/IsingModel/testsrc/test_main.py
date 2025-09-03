8b749dc9abda3d020505379cb4629c394a44dc8d
Gareth Aneurin Tribello <garethtribello@gareths-mbp-4.mp.qub.ac.uk>
2025-09-03 08:54:01 +0100

    Fixes for Ising model exercises

--- New-MTH4332/IsingModel/testsrc/test_main.py
+++ New-MTH4332/IsingModel/testsrc/test_main.py
@@ -39,7 +39,7 @@ class UnitTests(unittest.TestCase) :
             for j in range(30) : 
                 ispin = int(np.floor( N*np.random.uniform(0,1) ) )
                 jspin = int(np.floor( N*np.random.uniform(0,1) ) )
-                inputs.append((spins,ispin,jspin,))
+                inputs.append((spins.copy(),ispin,jspin,))
                 spins[ispin,jspin] = -1*spins[ispin,jspin]
                 outputs.append(spins)
         assert( check_func("flipSpin", inputs, outputs ) )
@@ -50,7 +50,7 @@ class UnitTests(unittest.TestCase) :
             for k in range(3) :
                 spins = genSpins( N ) 
                 for j in range(2) :
-                    inputs.append((spins,))
+                    inputs.append((spins.copy(),))
                     spins = -1*spins
                     output.append(spins) 
         assert( check_func("flipAllSpins", inputs, outputs ) )
@@ -59,7 +59,7 @@ class UnitTests(unittest.TestCase) :
         inputs, outputs = [], []
         for N in range(3,8) :
             spins = np.ones([N,N])
-            inputs.append((spins,))
+            inputs.append((spins.copy(),))
             p = 1/(1+N*N)
             myvar = randomvar( p, variance=p*(1-p), vmin=0, vmax=1, isinteger=True, nsamples=100 )
             outputs.append( myvar )
