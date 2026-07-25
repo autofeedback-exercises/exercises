283ec87b7bf18a2c78d6eee0fd1dc4f5b55b34aa
Gareth Tribello <garethtribello@Mac.lan>
2026-07-25 13:07:57 +0100

    More start of material for hypothesis testing exercises

---
+++ Hypothesis/T-tests/testsrc/test_main.py
@@ -0,0 +1,9 @@
+from AutoFeedback.funcchecks import check_func
+from AutoFeedback.plotchecks import check_plot
+from AutoFeedback.plotclass import line
+from AutoFeedback.utils import get_internal as get
+from AutoFeedback.randomclass import randomvar
+import numpy as np
+import unittest
+
+class UnitTests(unittest.TestCase):
