d3e240ce2e7538a9c8b3a982082a1b7ad9dcbb5d
Gareth Tribello <garethtribello@gareths-mbp.mp.qub.ac.uk>
2026-08-19 11:47:38 +0100

    More fixes to new exercise on operators and objects

--- Objects/Operators/testsrc/test_main.py
+++ Objects/Operators/testsrc/test_main.py
@@ -134,7 +134,7 @@ class UnitTests(unittest.TestCase):
     def test_ex12a(self):
         inputs, outputs = [], []
         for i in range(3,10) : 
-            myset = set( np.linspace(1,i,i) )
+            myset = list( np.linspace(1,i,i) )
             inputs.append((myset,))
             outputs.append(set( itertools.combinations(myset, 2 )))
         assert check_func("bounded_power_set_2", inputs, outputs )
@@ -142,7 +142,7 @@ class UnitTests(unittest.TestCase):
     def test_ex12b(self):
         inputs, outputs = [], []
         for i in range(3,10) :
-            myset = set( np.linspace(1,i,i) )
+            myset = list( np.linspace(1,i,i) )
             inputs.append((myset,))
             outputs.append(set( itertools.combinations(myset, 3 )))
         assert check_func("bounded_power_set_3", inputs, outputs )
@@ -150,7 +150,7 @@ class UnitTests(unittest.TestCase):
     def test_ex12c(self):
         inputs, outputs = [], []
         for i in range(3,10) :
-            myset = set( np.linspace(1,i,i) )
+            myset = list( np.linspace(1,i,i) )
             inputs.append((myset,))
             outputs.append(set( itertools.combinations(myset, 4 )))
         assert check_func("bounded_power_set_4", inputs, outputs )
